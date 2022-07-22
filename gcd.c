37328d6eac00e9186beb06903773eaae53fed0b0
imadh-a <93546632+imadh-a@users.noreply.github.com>
2022-07-22 10:44:47 +0530

    Update gcd.c

--- gcd.c
+++ gcd.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
-#include<time.h>
-void delay(){
-    for(int i=0;i<1000;i++){
-    for(int j=0;j<1000;j++){
-        for(int k=0;k<1000;k++);
-    }}}
-int gcd(int n1, int n2){
-    if(n2!=0)
-        gcd(n2,n1%n2);
-
-    else
-    return n1;
-}
+int gcd(int n1,int n2);
 int main()
 {
-     time_t st,ed;
-     st = time(NULL);
-    int a,b;
-    printf("Enter two numbers\n");
-    scanf("%d" "%d",&a,&b);
-    delay();
-    printf("Required GCD Value:%d",gcd(a,b));
-    ed = time(NULL);
-    printf("\n TIME TAKEN = %f", difftime(ed,st));
+    int n1,n2;
+    printf("Enter two positive integers:");
+    scanf("%d %d",&n1, &n2);
+    printf("GCD of %d and %d is %d",n1,n2,gcd(n1,n2));
+    return 0;
+}
+int gcd(int n1, int n2)
+{
+    if(n2!=0)
+    return gcd(n2,n1%n2);
+    else
+    {
+        return n1;
+}
     
 }
-
-
