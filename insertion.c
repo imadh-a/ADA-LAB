e237c3dd508316462c1e61655f94d0c9bfcd4648
imadh-a <93546632+imadh-a@users.noreply.github.com>
2022-07-22 18:32:43 +0530

    Update insertion.c

--- insertion.c
+++ insertion.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<time.h>
 
 
-void printArray(int array[], int size) {
+/*void printarray(int array[], int size) {
   for (int i = 0; i < size; i++) {
     printf("%d ", array[i]);
   }
   printf("\n");
-}
+}*/
 
-void insertionSort(int array[], int size) {
-  for (int step = 1; step < size; step++) {
-    int key = array[step];
-    int j = step - 1;
+void insertionsort(int array[], int size) {
+  for (int i = 1; i < size; i++) {
+    int key = array[i];
+    int j = i - 1;
     while (key < array[j] && j >= 0) {
       array[j + 1] = array[j];
       --j;
@@ -20,18 +23,111 @@ void insertionSort(int array[], int size) {
   }
 }
 
+int main()
 
-int main() {
-  int size;
-  printf("enter size of array");
-  scanf("%d",&size);
-  int a[size];
-  printf("Enter elements ");
-  for(int i=0;i<size;i++){
-    scanf("%d",&a[i]);    
-  }
+{
+    int i,j,k,a[15000];
  
-  insertionSort(a, size);
-  printf("Sorted array in ascending order:\n");
-  printArray(a, size);
+   int ch,n;
+    
+clock_t start,end;
+ 
+   while(1)
+  
+  {
+        
+printf("\n1:for manual entry of n value and array elements");
+   
+     printf("\n2:to display time taken for sorting number of elements");
+     
+   printf("\n3:exit");
+        
+printf("\nenter your choice:");
+        
+scanf("%d",&ch);
+        
+        
+switch(ch)
+        {
+       
+     case 1:printf("\nenter the number of elements: ");
+       
+     scanf("%d",&n);
+           
+ printf("\nenter the array elements: ");
+       
+     for(i=0;i<n;i++)
+          
+  {
+               
+ scanf("%d",&a[i]);
+     
+        }
+        
+   start=clock();
+     
+      insertionsort(a,n);
+ 
+          end=clock();
+         
+  printf("\nsorted array");
+     for (int i = 0; i < n; i++) {
+    printf("%d ", a[i]);
+  }
+      //printarray(a,n);
+       
+    printf("\ntime taken to sort %d number of elements is %f secs",n,(((double)(end-start)/CLOCKS_PER_SEC)));
+ 
+          break;
+           
+    
+       case 2:
+           
+printf("\nrunning values from 500 to 14500");
+  
+         n=500;
+          
+ while(n<14500)
+          
+ {
+             
+  for(i=0;i<n;i++)
+
+               {
+         
+          a[i]=rand();
+    
+           }
+          
+     start=clock();
+      
+         insertionsort(a,n);
+  
+             for(j=0;j<5000;j++)
+ 
+              {
+                 
+  int temp=38/600;
+             
+  }
+               end=clock();
+   
+            printf("\ntime taken to sort %d numbers is %f secs",n,(((double)(end-start)/CLOCKS_PER_SEC)));
+   
+            n=n+500;
+               
+       
+    }
+          
+ break;
+           
+case 3:exit(0);
+  
+      }
+     
+   
+    }
+   
+ return 0;
+
 }
