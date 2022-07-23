481939c97e11b2402fed06a99117f621a8aafede
imadh-a <93546632+imadh-a@users.noreply.github.com>
2022-07-24 00:32:21 +0530

    Update topological.c

--- topological.c
+++ topological.c
@@ -1,63 +1,49 @@
 #include<stdio.h>
-
 #include<conio.h>
 
-int main(){
+void source_removal(int n, int a[10][10]) {
+  int i, j, k, u, v, top, s[10], t[10], indeg[10], sum;
+  for(i = 0; i < n; i++) {
+    sum = 0;
+    for(j = 0; j < n; j++) {
+      sum += a[j][i];
+    }
+    indeg[i]=sum;
+  }
+  top = -1;
+  for(i=0;i<n;i++) {
+    if(indeg[i] == 0) {
+      s[++top] = i;
+    }
+  }
+  k = 0;
+  while(top != -1) {
+    u = s[top--];
+    t[k++] = u;
+    for(v = 0; v < n; v++) {
+      if(a[u][v] == 1) {
+        indeg[v] = indeg[v] - 1;
+        if(indeg[v] == 0)
+        s[++top] = v;
+      }
+    }
+  }
 
-int i,j,k,n,a[10][10],indeg[10],flag[10],count=0;
-
-printf("Enter the no of vertices:\n");
-
-scanf("%d",&n);
-
-printf("Enter the adjacency matrix:\n");
-
-for(i=0;i<n;i++){
-
-printf("Enter row %d\n",i+1);
-
-for(j=0;j<n;j++)
-
-scanf("%d",&a[i][j]);
-
+  for(i = 0;i < n; i++) {
+    printf("%d\n", t[i]);
+  }
 }
 
-for(i=0;i<n;i++){
-
-        indeg[i]=0;
-
-        flag[i]=0;   }
-
-    for(i=0;i<n;i++)
-
-        for(j=0;j<n;j++)
-
-            indeg[i]=indeg[i]+a[j][i];
-
-    printf("\nThe topological order is:");
-
-    while(count<n){
-
-        for(k=0;k<n;k++){
-
-            if((indeg[k]==0) && (flag[k]==0)){
-
-                printf("%d ",(k+1));
-
-                flag [k]=1;
-
-            }
-
-            for(i=0;i<n;i++){
-
-                if(a[i][k]==1)
-
-                    indeg[k]--;
-
-            }    }
-
-     count++;
-
-    }   return 0;
-   
-} 
+void main() {
+  int i, j, a[10][10], n;
+  printf("Enter number of nodes\n");
+  scanf("%d", &n);
+  printf("Enter the adjacency matrix\n");
+  for(i = 0; i < n; i++) {
+    for(j = 0; j < n; j++) {
+      scanf("%d", &a[i][j]);
+    }
+  }
+  source_removal(n,a);
+  getch();
+}
