18c530d8a2a10da5d18fa50590ea1c1aa2ee4748
imadh-a <93546632+imadh-a@users.noreply.github.com>
2022-07-23 10:11:36 +0530

    Create lin&bin.c

---
+++ lin&bin.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int bin_srch(int[],int,int,int);
+int lin_srch(int[],int,int,int);
+int n,a[10000];
+int main(){
+    int ch,key,search_status,temp;
+    clock_t end,start;
+    unsigned long int i,j;
+    while(1){
+        printf("1.Binary search\t 2.Linear search\t 3.Exit\n");
+        printf("Enter your choice:\n");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1: n=1000;
+            while(n<=5000){
+                for(i=0;i<n;i++){
+                    a[i]=i;
+                }
+                key=a[n-i];
+                start=clock();
+                search_status=bin_srch(a,0,n-1,key);
+                if(search_status==-1){
+                    printf("Key not found\n");
+                }
+                else{
+                    printf("Key found at %d location\n",search_status);
+                }
+                for(j=0;j<50000000;j++){
+                    temp=38/600;}
+                    end=clock();
+                    printf("\nTime taken for n=%d is %f secs",n,(((double)(end-start))/CLOCKS_PER_SEC));
+                    n=n+1000;
+                
+                }
+                break;
+                case 2: n=1000;
+            while(n<=5000){
+                for(i=0;i<n;i++){
+                    a[i]=i;
+                }
+                key=a[n-i];
+                start=clock();
+                search_status=bin_srch(a,0,n-1,key);
+                if(search_status==-1){
+                    printf("Key not found\n");
+                }
+                else{
+                    printf("Key found at %d location\n",search_status);
+                }
+                for(j=0;j<50000000;j++){
+                    temp=38/600;}
+                    end=clock();
+                    printf("\nTime taken for n=%d is %f secs",n,(((double)(end-start))/CLOCKS_PER_SEC));
+                    n=n+1000;
+                
+                }
+                break;
+                case 3:exit(0);
+        }
+        getchar();
+    }
+}
+int bin_srch(int a[],int low, int high,int key){
+    int mid;
+    if(low>high){
+        return -1;}
+        mid=(low+high)/2;
+        if(key==a[mid]){
+            return mid;
+        }
+        if(key<a[mid]){
+            return bin_srch(a,low,mid-1,key);}
+            else{
+                return bin_srch(a,mid+1,high,key);}
+            }
+int lin_srch(int a[],int i, int high,int key){
+    if(i>high){
+        return -1;
+    }
+    if(key==a[i]){
+        return i;
+    }
+    else{ return lin_srch(a,i+1,high,key);
+}
+}    
