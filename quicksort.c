b4884ef1c9c8e2173bbd3e98a7e5b1573904fb35
afifah2002 <94037360+afifah2002@users.noreply.github.com>
2022-07-23 13:19:34 +0530

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,61 +1,125 @@
-#include <stdio.h>  
+#include<stdio.h>
+#include<conio.h>
 #include<time.h>
-int partition (int a[], int start, int end)  
-{  
-    int pivot = a[end]; // pivot element  
-    int i = (start - 1);  
- 
-    for (int j = start; j <= end - 1; j++)  
-    {  
-        // If current element is smaller than the pivot  
-        if (a[j] < pivot)  
-        {  
-            i++; // increment index of smaller element  
-            int t = a[i];  
-            a[i] = a[j];  
-            a[j] = t;  
-        }  
-    }  
-    int t = a[i+1];  
-    a[i+1] = a[end];  
-    a[end] = t;  
-    return (i + 1);  
-}  
- 
-void quick(int a[], int start, int end)
-{  
-    if (start < end)  
-    {  
-        int p = partition(a, start, end);
-        quick(a, start, p - 1);  
-        quick(a, p + 1, end);  
-    }  
-}  
- 
- 
-void printArr(int a[], int n)  
-{  
-    int i;  
-    for (i = 0; i < n; i++)  
-        printf("%d ", a[i]);  
-}  
-int main()  
-{  int n;time_t st,ed;
-printf("enter size of array\n");
+#include<stdlib.h>
+int partition(int a[],int low,int high)
+{
+    int key,i,j,temp;
+    key=a[low];
+    i=low+1;
+    j=high;
+    while(1)
+    {
+        while(i<high && key>=a[i])
+            i++;
+        while(key<a[j])
+            j--;
+        if(i<j)
+        {
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+        }
+        else
+        {
+            temp=a[low];
+            a[low]=a[j];
+            a[j]=temp;
+            return j;
+        }
+    }
+}
+void quicksort(int a[],int low,int high)
+{
+    int j;
+    if(low<high)
+    {
+        j=partition(a,low,high);
+        quicksort(a,low,j-1);
+        quicksort(a,j+1,high);
+    }
+}
+void main()
+{
+int a[15000],n,i,j,ch,temp;
+clock_t start,end;
+while(1)
+{
+printf("\n1.For manual entry of N value and array elements");
+printf("\n2.To display time taken for sorting number elements N in the range 500 to 14500");
+printf("\n3.To exit");
+printf("\nEnter your choice");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1:
+printf("\nEnter the number of elements:");
 scanf("%d",&n);
-    int a[n];
-    printf("enter array elements\n");
-    for(int i=0;i<n;i++)
-    scanf("%d",&a[i]);
-   
-    printf("Before sorting array elements are - \n");  
-    printArr(a, n);
-    st=time(NULL);
-    quick(a, 0, n - 1);
-    ed=time(NULL);
-    printf("\nAfter sorting array elements are - \n");    
-    printArr(a, n);  
-    printf("time taken=%f",difftime(ed,st));
-     
-    return 0;  
-}  
+printf("\nEnter array elements:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+start=clock();
+quicksort(a,0,n-1);
+end=clock();
+printf("\nSorted array is:");
+for(i=0;i<n;i++)
+printf("%d\t",a[i]);
+printf("\n Time taken to sort %d numbers is %f Secs",n, (((double)(end-start))/CLOCKS_PER_SEC));
+break;
+
+case 2:
+n=500;
+while(n<=15000)
+{
+for(i=0;i<n;i++)
+{
+a[i]=n-i;
+}
+start=clock();
+quicksort(a,0,n-1);
+
+for(j=0;j<50000000;j++)
+{
+temp=38/600;
+}
+end=clock();
+printf("\n Time taken to sort %d numbers is %f Secs",n,(((double)(end-start))/CLOCKS_PER_SEC));
+n=n+1000;
+}
+break;
+
+case 3:exit(0);
+}
+getchar();
+}
+}
+
+/*
+
+void main()
+{
+    int a[10000],n,t,i;
+    clock_t end,start;
+   \
+    printf("Enter the number of array elements:\n");
+    scanf("%d",&n);
+    printf("Enter the array elements:\n");
+    for(i=0;i<n;i++)
+    {
+      a[i]=rand()%1000;
+      printf("%d\n",a[i]);
+    }
+    start=clock();
+    for(int j=0;j<5000000;j++)
+        t=900/900;
+    quicksort(a,0,n-1);
+     end=clock();
+    printf("Sorted array:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+     printf("Time taken to search an given element in an array of is %f\n",(((double)(end-start))/CLOCKS_PER_SEC));
+}*/
