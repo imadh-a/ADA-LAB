a3a6d4aa63b96680a0f7498784da8e7bd9b73c78
imadh-a <93546632+imadh-a@users.noreply.github.com>
2022-08-01 15:13:36 +0530

    Update warshall.c

--- warshall.c
+++ warshall.c
@@ -25,8 +25,9 @@ int main(){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             printf("%d ", r[n][i][j]);
+               printf("\n");
         }
-            printf("\n");
+          
         
     }
 }
