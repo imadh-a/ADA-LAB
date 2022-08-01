019b5d252ab21bc9aba365e1ebf1f628e4007eb3
imadh-a <93546632+imadh-a@users.noreply.github.com>
2022-08-01 22:54:37 +0530

    Update heapsort.c

--- heapsort.c
+++ heapsort.c
@@ -5,62 +5,61 @@
 void swap(int *,int *);
 void heapify(int [],int,int);
 void heapSort(int[], int);
-int main()
+void main()
 {
-    int a[15000], n, i, j, ch, temp;
-    clock_t start, end;
-    while (1)
+    int a[15000],n,i,j,ch,temp;
+    clock_t start,end;
+    while(1)
     {
-        printf("\n1:FOR MANUAL ENTRY");
-        printf("\n2:DISPLAY TIME TAKEN TO SORT ELEMENTS FROM RANGE 500 TO 15000");
-        printf("\n3:EXIT");
-        printf("\nENTER YOUR CHOICE:");
-        scanf("%d", &ch);
-        switch (ch)
+        printf("\n 1: For manual entry of N values and array elements:");
+        printf("\n 2: To display time taken for sorting number of elements N in the range 500 to 14500:");
+        printf("\n 3: To exit");
+        printf("\n Enter your choice:");
+        scanf("%d",&ch);
+        switch(ch)
         {
-        case 1:
-            printf("\nENTER NUMBER OF ARRAY ELEMENTS: ");
-            scanf("%d", &n);
-            printf("\nENTER ARRAY ELEMENTS: ");
-            for (i = 0; i < n; i++)
+            case 1: printf("\n Enter the number of elements:");
+            scanf("%d",&n);
+            printf("\n Enter array elements:");
+            for(i=0;i<n;i++)
             {
-                scanf("%d", &a[i]);
+                scanf("%d",&a[i]);
             }
-            start = clock();
+            start=clock();
             heapSort(a, n);
-            end = clock();
-            printf("\nSORTED ARRAY IS: ");
-            for (i = n-1; i >= 0; i--)
-                printf("%d\t", a[i]);
-            printf("\n TIME TAKEN TO SORT %d NUMBERS IS %f SECS", n, (((double)(end - start)) / CLOCKS_PER_SEC));
-            break;
-        case 2:
-            n = 500;
-            while (n <= 14500)
-            {
-                for (i = 0; i < n; i++)
-                {
-                     //a[i]=rand()%n;
-                    a[i] = n - i;
-                }
-                start = clock();
-                heapSort(a, n);
-                for (j = 0; j < 50000000; j++)
-                {
-                    temp = 38 / 600;
-                }
-                end = clock();
-                printf("\n TIME TAKEN TO SORT %d NUMBERS IS %f SECS", n, (((double)(end - start)) / CLOCKS_PER_SEC));
-                n = n + 1000;
+            end=clock();
+            printf("\n Sorted array is:");
+            for(i=n-1;i>=0;i--){
+            printf("%d\t",a[i]);
             }
+            printf("\n Time taken to sort %d numbers is %f secs",n,((double)(end-start)/CLOCKS_PER_SEC));
             break;
-        case 3:
-            exit(0);
+            case 2:
+            n=500;
+            while(n<=14500){
+                for(i=0;i<n;i++){
+                    a[i]=n-i;
+                    
+                }
+                start=clock();
+                 heapSort(a, n);
+                for(j=0;j<500000;j++){
+                    temp=38/600;
+                    
+                }
+                end=clock();
+                printf("\n Time taken to sort %d numbers is %f secs",n,((double)(end-start)/CLOCKS_PER_SEC));
+                n=n+1000;
+                 
+                }
+                break;
+                
+            
+            case 3: exit(0);
         }
-        getchar();
+        
     }
 }
-
 void swap(int *a, int *b)
 {
     int temp = *a;
